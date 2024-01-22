f636c44dc464d655722e2ac4e23c87e67bdef1f7
Anton Khassanov <marioanton9@gmail.com>
2024-01-22 15:54:57 -0500

    [UPDATE] Commented Root Files of Project

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import AboutMePage from "./components/AboutMePage"
 import Services from "./components/Services" 
 import ContactInformation from "./components/ConactInformation";
 
-
+// Root component with all other components nested within.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
