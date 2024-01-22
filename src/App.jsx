1c4365f9a2bcd8dab63822effc6bea51abd88bd9
Anton Khassanov <marioanton9@gmail.com>
2024-01-22 15:52:03 -0500

    current project version

---
+++ src/App.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './App.css';
+
+
+
+function App() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+
+  const handleLogin = async (e) => {
+    e.preventDefault()  
+    
+    
+      navigate('/home')
+  
+  };
+
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.heading}>Power Through Healing </h1> 
+    
+      <div style={styles.formContainer}>
+       
+      <img 
+      src="PTH Logo.png" 
+      alt="Lotus Flower" 
+      style={styles.lotusImage}
+      />
+
+       <form onSubmit={handleLogin}>
+        
+        <label style={styles.label}>
+          Username:
+          <input 
+          type="text" 
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)} 
+          style={styles.input} 
+          
+          />
+
+        </label>
+        <label style={styles.label}>
+
+          Password:
+
+        <input 
+        type="password" 
+        value={password} 
+        onChange={(e) => setPassword(e.target.value)} 
+        style={styles.input} 
+        required
+        />
+        </label>
+        
+        <button type='submit' style={styles.button}>Login</button>
+    
+    </form>
+          
+      </div>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    background: 'linear-gradient( 45deg,#B8E994, #FDA7DF, #FF6B6B, #57BC90)', 
+  },
+  heading: {
+    fontFamily: 'cursive',
+    fontSize: '3em',
+    color: '#FFF',
+    marginBottom: '15px',
+  },
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '300px',
+    padding: '20px',
+    borderRadius: '10px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+    background: '#FFF', 
+  },
+  lotusImage: {
+    width: '80px', 
+    height: 'auto',
+    marginBottom: '20px',
+  },
+  label: {
+    display: 'block',
+    margin: '15px 0',
+    color: '#2C3A47', 
+  },
+  input: {
+    width: '90%',
+    padding: '12px',
+    borderRadius: '5px',
+    border: '1px solid #95A5A6', 
+    marginBottom: '20px',
+    fontSize: '1em',
+  },
+  button: {
+    width: '100%',
+    padding: '12px',
+    borderRadius: '5px',
+    background: '#FF6B6B',
+    color: '#FFF', 
+    cursor: 'pointer',
+    fontSize: '1em',
+  },
+  tokenContainer: {
+    marginTop: '20px',
+  },
+  token: {
+    fontFamily: 'monospace',
+    fontSize: '1em',
+    color: '#2C3A47', 
+  },
+};
+
+
+
+
+export default App;
---
+++ src/components/AboutMePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMePage() {
+    return (
+        <div>
+            <h1> Welcome to the AboutMe Page!</h1>
+        </div>
+    )
+}
+
+
+export default AboutMePage
